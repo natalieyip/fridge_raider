64276c7fab12a23bde832506f97e726789fff4a1
Natalie Yip <yip.natalie92@gmail.com>
2017-05-21 16:36:37 -0700

    Add a split ingredient method in Ingredient class

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,18 @@
 class Ingredient < ApplicationRecord
 	has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+
+  def split_ingredients(ingredients)
+  	individual_ingredients = ingredients.split(", ")
+  	all_ingredients = Ingredient.all
+  	individual_ingredients.each do |user_ingredient| 
+  		all_ingredients.each do |recipe_ingredients| 
+  			if recipe_ingredients.item.include?(user_ingredient)
+  				p recipe_ingredients.recipes
+  			end 
+  		end 
+  	end 
+  end
+
+
 end
